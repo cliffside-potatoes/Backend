4fd23424d3fa2187c848082895a29c601202ca06
parking-been <ysbeen00@gmail.com>
2026-01-02 17:28:38 +0900

    chore: initial commit

---
+++ src/main/java/com/potatoes/Naengu/NaenguApplication.java
@@ -0,0 +1,13 @@
+package com.potatoes.Naengu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaenguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaenguApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/potatoes/Naengu/NaenguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potatoes.Naengu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaenguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
