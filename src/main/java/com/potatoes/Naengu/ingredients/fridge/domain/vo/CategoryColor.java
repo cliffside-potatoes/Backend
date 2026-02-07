d1ea875579640260f709aab29c45831e2dd40bb8
sejin <psj8880@naver.com>
2026-02-07 19:08:15 +0900

    feat(CategoryColor): enum으로 구현
    - 추후 협의된 색상으로 enum value 수정 가능

---
+++ src/main/java/com/potatoes/Naengu/ingredients/fridge/domain/vo/CategoryColor.java
@@ -0,0 +1,16 @@
+package com.potatoes.Naengu.ingredients.fridge.domain.vo;
+
+import lombok.Getter;
+
+@Getter
+public enum CategoryColor {
+    RED("#FF0000"),
+    GREEN("#00FF00"),
+    BLUE("#0000FF");
+
+    private final String rgb;
+
+    CategoryColor(String rgb) {
+        this.rgb = rgb;
+    }
+}
