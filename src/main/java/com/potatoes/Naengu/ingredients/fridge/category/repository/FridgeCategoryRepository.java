67b5ca654a9b3b7d2614180ad8ab0fdf939a949c
sejin <psj8880@naver.com>
2026-02-07 19:28:35 +0900

    feat(FridgeCategoryRepository): 카테고리 중복 체크 및 저장타입별 orderIndex 조회 메서드 추가
    - fridgeId + storageType + name 기준 중복 여부 확인
    - fridgeId + storageType 기준 회대 orderIndex 조회 쿼리 추가

---
+++ src/main/java/com/potatoes/Naengu/ingredients/fridge/category/repository/FridgeCategoryRepository.java
@@ -0,0 +1,23 @@
+package com.potatoes.Naengu.ingredients.fridge.category.repository;
+
+import com.potatoes.Naengu.ingredients.fridge.domain.model.category.FridgeCategory;
+import com.potatoes.Naengu.ingredients.fridge.domain.vo.StorageType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface FridgeCategoryRepository extends JpaRepository<FridgeCategory, Long> {
+
+    boolean existsByFridgeIdAndStorageTypeAndName(
+            Long fridgeId,
+            StorageType storageType,
+            String name
+    );
+
+    @Query("""
+ select coalesce(max(c.orderIndex), 0)
+ from FridgeCategory c
+ where c.fridgeId = :fridgeId
+   and c.storageType = :storageType
+""")
+    int findMaxOrderIndexByFridgeIdAndStorageType(Long fridgeId, StorageType storageType);
+}
