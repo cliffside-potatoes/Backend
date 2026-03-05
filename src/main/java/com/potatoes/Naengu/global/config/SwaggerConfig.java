7b8aac02188c9218dad1f3d60ce494f045e4474b
parking-been <ysbeen00@gmail.com>
2026-03-05 23:25:30 +0900

    feat: swagger ui cors 문제 해결

--- src/main/java/com/potatoes/Naengu/global/config/SwaggerConfig.java
+++ src/main/java/com/potatoes/Naengu/global/config/SwaggerConfig.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,9 +15,13 @@ public class SwaggerConfig {
 
     private static final String BEARER_AUTH = "bearerAuth";
 
+    @Value("${app.server-url}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url(serverUrl).description("API Server"))
                 .info(new Info()
                         .title("냉구 API")
                         .description("냉구 서비스 API 명세서")
