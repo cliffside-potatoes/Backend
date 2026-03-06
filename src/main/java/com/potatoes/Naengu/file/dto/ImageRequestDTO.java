ad4169d0f9cea7aa921f3dee05ee013b0ec775f4
ysbeen00@gmail.com <ysbeen00@gmail.com>
2026-03-06 09:35:12 +0900

    feat: swagger ui 설정 완료

--- src/main/java/com/potatoes/Naengu/file/dto/ImageRequestDTO.java
+++ src/main/java/com/potatoes/Naengu/file/dto/ImageRequestDTO.java
@@ -1,5 +1,6 @@
 package com.potatoes.Naengu.file.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ImageRequestDTO {
+    @Schema(description = "업로드할 파일명", example = "photo.jpg")
     private String imageName;
 }
