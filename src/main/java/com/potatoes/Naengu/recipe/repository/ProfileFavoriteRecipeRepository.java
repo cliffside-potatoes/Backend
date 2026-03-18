08dfa11d065a9e19310b5efce8b4882160bc52bc
sejin <psj8880@naver.com>
2026-03-18 20:53:27 +0900

    feat: 레시피 찜 생성 api 구현

---
+++ src/main/java/com/potatoes/Naengu/recipe/repository/ProfileFavoriteRecipeRepository.java
@@ -0,0 +1,11 @@
+package com.potatoes.Naengu.recipe.repository;
+
+import com.potatoes.Naengu.profile.domain.model.Profile;
+import com.potatoes.Naengu.recipe.domain.model.ProfileFavoriteRecipe;
+import com.potatoes.Naengu.recipe.domain.model.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfileFavoriteRecipeRepository extends JpaRepository<ProfileFavoriteRecipe, Long> {
+
+    boolean existsByProfileAndRecipe(Profile profile, Recipe recipe);
+}
